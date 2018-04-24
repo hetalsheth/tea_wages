d63f8cf10ab9a00a45d62be7a820f4e713315455
Administrator <nivedita12011990@gmail.com>
2018-04-24 16:24:37 +0530

    validation for wage rate

--- tea_wages/tea_wages/doctype/tearp_wages_entry/tearp_wages_entry.js
+++ tea_wages/tea_wages/doctype/tearp_wages_entry/tearp_wages_entry.js
@@ -9,7 +9,36 @@ frappe.ui.form.on('Tearp Wages Entry', {
 
 frappe.ui.form.on('Tearp Wages Entry', 'validate', function(frm) {
   for(var i in frm.doc.attendance){
+		 frm.doc.attendance[i].record_date = frm.doc.record_date;
      frm.doc.attendance[i].garden = frm.doc.garden;
-		 frm.doc.attendance[i].book_code = frm.doc.book_code
+		 frm.doc.attendance[i].book_code = frm.doc.book_code;
      }
   });
+
+frappe.ui.form.on('Tearp Wages Entry', 'validate', function(frm) {
+	frm.naming_series = '';
+	name = frm.doc.garden+"/"+frm.doc.book_code+"/"+frm.doc.record_date;
+	frm.set_value('naming_series', name);
+  });
+
+
+frappe.ui.form.on('Tearp Wages Entry', 'validate', function(frm) {
+	var rate = 137.50;
+	for(var i in frm.doc.attendance){
+		if(frm.doc.attendance[i].attendance_type == 'Full Day'){
+			frm.doc.attendance[i].wage_rate = rate;
+		}
+		else if (frm.doc.attendance[i].attendance_type == 'Paid Leave') {
+			frm.doc.attendance[i].wage_rate = rate;
+		}
+		else if (frm.doc.attendance[i].attendance_type == 'Maternity Leave') {
+			frm.doc.attendance[i].wage_rate = rate;
+		}
+		else if (frm.doc.attendance[i].attendance_type == 'Half Day') {
+			frm.doc.attendance[i].wage_rate = rate/2;
+		}
+		else if (frm.doc.attendance[i].attendance_type == 'Sick Leave') {
+			frm.doc.attendance[i].wage_rate = (rate*(2/3)).toFixed(2);
+		}
+	}
+	 });
